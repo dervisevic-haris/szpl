3a8f5551264b0cedccf572609851790471a260c8
dervisevic-haris <90dervisevic.haris@gmail.com>
2013-11-26 14:05:42 +0100

    AdminController i Tablesorter Plugin

--- szpl/app/views/ShowUsersView.blade.php
+++ szpl/app/views/ShowUsersView.blade.php
@@ -41,6 +41,25 @@ $("#Change").on("click", function(e){
     });
     });
 
+  $("#Delete").on('click', function(event) {
+    event.preventDefault();
+        $.ajax({
+          url: '/path/to/file',
+          type: 'POST',
+          dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+          data: {param1: 'value1'},
+        })
+        .done(function() {
+          console.log("success");
+        })
+        .fail(function() {
+          console.log("error");
+        })
+        .always(function() {
+          console.log("complete");
+        });
+        
+  });
                 
   });
 
