e7cc089ee93387fa8b677eb5f5e3c361022d4337
dervisevic-haris <90dervisevic.haris>
2013-12-12 15:51:33 +0100

    test

--- szpl/app/config/database.php
+++ szpl/app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'szpl',
 			'username'  => 'root',
-			'password'  => 'aa',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
